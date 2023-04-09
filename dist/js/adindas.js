440e6f87f59dcae4aea977ea6a1e014c42bf2147
DafiNMaulana <unoobcombro@gmail.com>
2023-04-10 03:15:47 +0700

    add: header, statistic card, chart.js

--- dist/js/adindas.js
+++ dist/js/adindas.js
@@ -5,11 +5,30 @@ const menuClose = document.querySelector('.menu-open');
 const menuOpen = document.querySelector('.menu-close');
 const btnDots = document.getElementById('btn-sub-menu');
 const subMenu = document.querySelectorAll('.sub-menu-item');
+const profile = document.getElementById('profile');
+const profileMenu = document.getElementById('profile-menu');
+const notifParent = document.querySelector('.notif-parent');
+const notifBtn = document.getElementById('notif-btn');
 
 sidebarText.forEach(st => st.classList.add('transition-all', 'duration-300'));
 
+profile.addEventListener('click', () => {
+  profileMenu.classList.toggle('invisible');
+  profileMenu.classList.toggle('visible');
+  profileMenu.classList.toggle('top-[70px]');
+  profileMenu.classList.toggle('opacity-0');
+  profileMenu.classList.toggle('opacity-100');
+})
+
+notifBtn.addEventListener('click', () => {
+  notifParent.classList.toggle('invisible');
+  notifParent.classList.toggle('visible');
+  notifParent.classList.toggle('opacity-0');
+  notifParent.classList.toggle('opacity-100');
+})
+
 const toggleSidebar = () => {
-  sidebar.classList.toggle('w-[300px]');
+  sidebar.classList.toggle('w-[250px]');
   sidebar.classList.toggle('w-[100px]');
   sidebarText.forEach(st => {
     st.classList.toggle('opacity-0');
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
         'inter': ['Inter', 'sans-serif', 'ui-sans-serif', 'system-ui'],
       },
       colors: {
-        primary: "#37306B",
-        neutral: "#eeee",
-        accent: "#2E2760",
-        'base-100': '#6D63BC',
-        'base-200': '#5846E0'
+        'base-400': '#3953DF',
+        'base-450': '#2F47C7',
+        'success-card': '#358D6D',
+        'danger-card': '#A54141',
+        'info-card': '#258C86',
       },
     },
   },
@@ -19,5 +19,21 @@ module.exports = {
     require('daisyui'),
     require('tailwind-scrollbar-hide'),
   ],
+  daisyui: {
+    themes: [
+      {
+        adindasUI: {
+          primary: "#37306B",
+          neutral: "#eeee",
+          accent: "#2E2760",
+          'base-100': '#6D63BC',
+          'base-200': '#5846E0',
+          'base-300': '#534999',
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
 
