d1a0d5523a8a68574dbc7f124a52dbc78f9297f8
DafiNMaulana <unoobcombro@gmail.com>
2023-04-11 19:31:34 +0700

    fixed: chartJS responsive, add: modals(still develop)

--- dist/js/adindas.js
+++ dist/js/adindas.js
@@ -1,15 +1,50 @@
 const sidebar = document.getElementById('sidebar');
-const sidebarText = [...document.querySelectorAll('.sidebar-text')];
 const btnMenu = document.getElementById('btn-menu');
-const menuClose = document.querySelector('.menu-open');
-const menuOpen = document.querySelector('.menu-close');
-const btnDots = document.getElementById('btn-sub-menu');
-const subMenu = document.querySelectorAll('.sub-menu-item');
+const chart = document.getElementById('line-chart');
+const sidebarText = document.querySelectorAll('.sidebar-text');
 const profile = document.getElementById('profile');
 const profileMenu = document.getElementById('profile-menu');
 const notifParent = document.querySelector('.notif-parent');
 const notifBtn = document.getElementById('notif-btn');
-sidebarText.forEach(st => st.classList.add('transition-all', 'duration-300'));
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenu.addEventListener('change', () => {
+  sidebar.classList.toggle('w-0');
+  sidebar.classList.toggle('w-[300px]');
+  sidebar.classList.toggle('p-0');
+  sidebar.classList.toggle('p-5');
+  sidebar.classList.toggle('overflow-x-hidden');
+  for(i of sidebarText) {
+    i.classList.toggle('scale-100');
+    i.classList.toggle('scale-0');
+  }
+  
+  if(mobileMenu.checked) {
+    btnMenu.checked = false
+  } else if (btnMenu.checked = false) {
+    btnMenu.checked = false
+  }
+})
+
+btnMenu.addEventListener('change', () => {
+  sidebar.classList.toggle('lg:w-[300px]');
+  sidebar.classList.toggle('lg:w-[100px]');
+  for(i of sidebarText) {
+    i.classList.toggle('lg:scale-100');
+    i.classList.toggle('lg:scale-0');
+  }
+  if (window.innerWidth <= 992) {
+    sidebar.classList.toggle('w-0');
+    sidebar.classList.toggle('w-[300px]');
+    sidebar.classList.toggle('p-0');
+    sidebar.classList.toggle('p-5');
+    sidebar.classList.toggle('overflow-x-hidden');
+    for(i of sidebarText) {
+      i.classList.toggle('scale-100');
+      i.classList.toggle('scale-0');
+    }
+  }
+});
 
 profile.addEventListener('click', () => {
   profileMenu.classList.toggle('invisible');
@@ -25,29 +60,3 @@ notifBtn.addEventListener('click', () => {
   notifParent.classList.toggle('opacity-0');
   notifParent.classList.toggle('opacity-100');
 })
-
-const toggleSidebar = () => {
-  if(window.matchMedia('(max-width: 992px)')) {
-    
-  }
-  sidebar.classList.toggle('w-[250px]');
-  sidebar.classList.toggle('w-[100px]');
-  sidebarText.forEach(st => {
-    st.classList.toggle('scale-0');
-    st.classList.toggle('scale-100');
-  });
-}
-
-
-btnMenu.addEventListener('change', toggleSidebar);
-
-if (btnDots) {
-  btnDots.addEventListener('change', () => {
-    subMenu.forEach(item => {
-      item.classList.toggle('opacity-0', !btnDots.checked);
-      item.classList.toggle('opacity-100', btnDots.checked);
-      item.classList.toggle('translate-y-[999px]', !btnDots.checked);
-      item.classList.toggle('translate-y-0', btnDots.checked);
-    });
-  });
-}
